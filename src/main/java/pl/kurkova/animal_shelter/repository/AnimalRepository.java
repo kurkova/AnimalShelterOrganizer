5f13239fd78edfad8b607b49ba8ae99ee292ef87
kurkova <kurkiewiczpaula@gmail.com>
2019-11-09 18:53:54 +0000

    Update name of method findBy_id in AnimalRepository

--- src/main/java/pl/kurkova/animal_shelter/repository/AnimalRepository.java
+++ src/main/java/pl/kurkova/animal_shelter/repository/AnimalRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import pl.kurkova.animal_shelter.domain.Animal;
 
 public interface AnimalRepository extends MongoRepository <Animal, String> {
-    Animal findBy(ObjectId _id);
+    Animal findBy_id(ObjectId _id);
 }
