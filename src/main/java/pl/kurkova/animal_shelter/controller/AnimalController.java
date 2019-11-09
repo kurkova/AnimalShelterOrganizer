70f41ca79d4d13cc6a36c62a7e1a37d0d5710784
kurkova <kurkiewiczpaula@gmail.com>
2019-11-09 19:25:57 +0000

    Add findAll method in Repository

---
+++ src/main/java/pl/kurkova/animal_shelter/controller/AnimalController.java
@@ -0,0 +1,4 @@
+package pl.kurkova.animal_shelter.controller;
+
+public class AnimalController {
+}
--- src/main/java/pl/kurkova/animal_shelter/repository/AnimalRepository.java
+++ src/main/java/pl/kurkova/animal_shelter/repository/AnimalRepository.java
@@ -4,8 +4,14 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import pl.kurkova.animal_shelter.domain.Animal;
 
+import java.util.List;
+
 public interface AnimalRepository extends MongoRepository <Animal, String> {
+
     Animal findById(ObjectId _id);
 
     Animal findByName (String name);
+
+    @Override
+    List<Animal> findAll();
 }
