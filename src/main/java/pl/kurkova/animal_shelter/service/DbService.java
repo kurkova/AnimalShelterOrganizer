9768d9c4f0b316e97dd062800b12188ee2877d46
kurkova <kurkiewiczpaula@gmail.com>
2019-11-09 22:08:54 +0000

    Create DbService class

--- src/main/java/pl/kurkova/animal_shelter/service/DbService.java
+++ src/main/java/pl/kurkova/animal_shelter/service/DbService.java
@@ -1,4 +1,26 @@
 package pl.kurkova.animal_shelter.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.kurkova.animal_shelter.domain.Animal;
+import pl.kurkova.animal_shelter.repository.AnimalRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class DbService {
+
+    @Autowired
+    private AnimalRepository repository;
+
+    public Optional<Animal>  getAnimalById(final String id){
+        return repository.findById(id);
+    }
+
+    public List<Animal> getAllAnimals(){
+        return repository.findAll();
+    }
 }
