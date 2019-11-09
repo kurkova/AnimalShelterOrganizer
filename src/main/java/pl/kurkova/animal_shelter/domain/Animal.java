7fab719481e8def01d143a0df9768f211f0a53c4
kurkova <kurkiewiczpaula@gmail.com>
2019-11-09 18:19:42 +0000

    Create Animal class

--- src/main/java/pl/kurkova/animal_shelter/domain/Animal.java
+++ src/main/java/pl/kurkova/animal_shelter/domain/Animal.java
@@ -1,4 +1,22 @@
 package pl.kurkova.animal_shelter.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Animal {
+    @Id
+    public ObjectId _id;
+
+    public String name;
+    public String species;
+    public String breed;
 }
