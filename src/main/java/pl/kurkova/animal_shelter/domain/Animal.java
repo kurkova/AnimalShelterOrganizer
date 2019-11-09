71dec8b26d490dbac0db3820323f13f72c9730cd
kurkova <kurkiewiczpaula@gmail.com>
2019-11-09 18:51:10 +0000

    Add name of the table animals

--- src/main/java/pl/kurkova/animal_shelter/domain/Animal.java
+++ src/main/java/pl/kurkova/animal_shelter/domain/Animal.java
@@ -7,11 +7,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Document(collection = "animals")
 public class Animal {
     @Id
     public ObjectId _id;
