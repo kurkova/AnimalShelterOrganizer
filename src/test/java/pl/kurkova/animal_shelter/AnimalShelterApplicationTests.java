f043a8ee8b84c2f97570607faabdccaa5cc166c4
kurkova <kurkiewiczpaula@gmail.com>
2019-11-09 18:05:28 +0000

    initial commit

---
+++ src/test/java/pl/kurkova/animal_shelter/AnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurkova.animal_shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
