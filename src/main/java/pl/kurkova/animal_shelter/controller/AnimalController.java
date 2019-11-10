de5ad9df2931fa167feb1ddaa4389ae1c2da8db1
kurkova <kurkiewiczpaula@gmail.com>
2019-11-10 09:09:47 +0000

    Update AnialController

--- src/main/java/pl/kurkova/animal_shelter/controller/AnimalController.java
+++ src/main/java/pl/kurkova/animal_shelter/controller/AnimalController.java
@@ -1,7 +1,6 @@
 package pl.kurkova.animal_shelter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,4 +27,6 @@ public class AnimalController {
     public Optional<Animal> getAnimalById(@RequestParam String id)  {
         return service.getAnimalById(id);
     }
+
+
 }
---
+++ src/main/java/pl/kurkova/animal_shelter/controller/exception/AnimalNotFoundException.java
@@ -0,0 +1,4 @@
+package pl.kurkova.animal_shelter.controller.exception;
+
+public class AnimalNotFoundException {
+}
