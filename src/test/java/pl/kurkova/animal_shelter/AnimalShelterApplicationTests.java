a11a52092dd376d8cd1537f58e7c0a63eed57a03
kurkova <kurkiewiczpaula@gmail.com>
2019-11-14 19:36:19 +0100

    Create shouldDeleteAnimal and shouldDeleteNoExistingAnimal method

--- src/test/java/pl/kurkova/animal_shelter/AnimalShelterApplicationTests.java
+++ src/test/java/pl/kurkova/animal_shelter/AnimalShelterApplicationTests.java
@@ -1,14 +1,14 @@
-//package pl.kurkova.animal_shelter;
-//
-//import org.junit.Test;
-////import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class AnimalShelterApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package pl.kurkova.animal_shelter;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
